a846ddd9a00207a2bd4d51e22e901c927fd04807
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2021-12-09 15:33:22 +0100

    add cicle for and testing

--- js/script.js
+++ js/script.js
@@ -30,3 +30,25 @@ const team = [
     image: 'barbara-ramos-graphic-designer.jpg',
   },
 ];
+// console.log(team);
+
+// Prendendo come riferimento la card di esempio presente nell’html, stampiamo dinamicamente una card per ogni membro del team.
+
+// seleziono il mio team-container e all'interno andrò a creare la mia card
+
+const teamContainer = document.querySelector('.team-container');
+console.log(teamContainer);
+
+// creo un ciclo for per creare le card in base hai nomi presenti nell'array
+for (let i = 0; i < team.length; i++) {
+  const member = team[i];
+  // console.log(member);
+  // creo dei div a cui darò la classe team-card
+  const teamCard = document.createElement('div');
+  teamCard.classList.add('team-card');
+  teamContainer.appendChild(teamCard);
+  // console.log(teamCard);
+  // creo attraverso i backtick la struttura 
+
+  
+}
